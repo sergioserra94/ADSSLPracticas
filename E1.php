eacb6cf80cf289ad97f468d507dfc73d306419b0
sergioserra94 <sergioserrano38@gmail.com>
2015-09-25 01:27:56 +0200

    Create E1.php

---
+++ E1.php
@@ -0,0 +1,17 @@
+<?php
+	$numero = mt_rand(100,200);
+	
+?>
+<html>
+<a><b><u>Numero aleatorio entre 100 y 200, ambos inlcusive</u></b></a>
+</br>
+</br>
+<a><b><?php echo $numero;?></b></a>
+</br>
+</br>
+<form action="E1.php">
+<input type="submit" value="Actualizar"></input>
+</form>
+
+</html>
+
