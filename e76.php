eedccea967a6bd1edc777e2597803827449cc87e
sergioserra94 <sergioserrano38@gmail.com>
2015-09-25 01:34:12 +0200

    Create e76.php

---
+++ e76.php
@@ -0,0 +1,16 @@
+<?php
+	$numero = mt_rand(100,200);
+	
+?>
+<html>
+<a><b><u>Numero aleatorio entre 100 y 200, ambos inlcusive</u></b></a>
+</br>
+</br>
+<table>
+<h1><?php echo $numero;?>, integer</h1>
+</table>
+<form action="e76.php">
+<input type="submit" value="Actualizar"></input>
+</form>
+
+</html>
