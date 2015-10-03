8706bb574dcd70f30551c507e2a928119b8a4ce6
sergioserra94 <sergioserrano38@gmail.com>
2015-10-03 14:59:07 +0200

    Create e65.php

---
+++ e65.php
@@ -0,0 +1,40 @@
+<?php
+
+$numero1=mt_rand(1,100);
+
+$numero2=mt_rand(1,100);
+
+$numero3=mt_rand(1,100);
+
+$numero4=mt_rand(1,100);
+
+$numero5=mt_rand(1,100);
+
+$numero6=mt_rand(1,100);
+
+$numero7=mt_rand(1,100);
+
+$numero8=mt_rand(1,100);
+
+$numero9=mt_rand(1,100);
+
+$numero10=mt_rand(1,100);
+?>
+
+
+<html>
+<head>
+	<title></title>
+</head>
+
+<body>
+
+<form action="e65.php">
+
+<P><?php echo "$numero1,$numero2,$numero3,$numero4,$numero5,$numero6,$numero7,$numero8,$numero9,$numero10";?></P>
+</br>
+</br>
+<input type="submit" value="Actualizar" />
+</form>
+</body>
+</html>
