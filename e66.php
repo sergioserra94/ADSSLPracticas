63bdb587ed1e27a1f141c8ceb11dc10bf143aadc
sergioserra94 <sergioserrano38@gmail.com>
2015-10-03 15:00:26 +0200

    Create e66.php

---
+++ e66.php
@@ -0,0 +1,22 @@
+<?php
+
+$numero=mt_rand(0,10);
+
+if($numero%2==0){
+
+?>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<p><?php echo "El numero $numero es par"; ?></p>
+
+<p><?php }else{  echo "El numero $numero es impar"; }?></p>
+</br>
+</br>
+
+</body>
+</html>
