4918e72b33a29d4574a8c9cbae32d63b735e8d5f
sergioserra94 <sergioserrano38@gmail.com>
2015-10-03 15:04:01 +0200

    Create e71.php

---
+++ e71.php
@@ -0,0 +1,16 @@
+<?php
+
+$retamar="Retamar";
+
+$letra=$retamar[mt_rand(0,6)];
+
+?>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p><?php  echo"Una letra aleatoria de la palabra Retamar es "?><b><?php echo " $letra" ?></b></p>
+</body>
+</html>
