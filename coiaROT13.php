6a7a8a995594b250c19820681ca4c81b72db9e2b
sergioserra94 <sergioserrano38@gmail.com>
2015-10-11 11:21:06 +0200

    Create coiaROT13.php

---
+++ coiaROT13.php
@@ -0,0 +1,26 @@
+<?php
+
+$palabra="";
+$cadena=$_GET["$palabra"];
+
+echo $cadena;
+
+for ($i=0; $i < strlen($cadena); $i++) {
+
+         if ($cadena[$i]!=' ') {
+
+         	if (chr(ord($cadena[$i])+13)<'z') {
+
+         		$cadena[$i]=chr(ord($cadena[$i])+13);
+         		
+         	}else{
+         		$cadena[$i]=chr(ord($cadena[$i])-13);
+
+         	}
+            
+}
+       
+}
+ echo $cadena;
+
+?>
